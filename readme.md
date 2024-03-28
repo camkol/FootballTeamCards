41ed92628bc64ebe5d1838dcc96efd11cf9c4861
camkol <121874225+camkol@users.noreply.github.com>
2024-03-28 14:40:26 +0100

    initial

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Football Team Cards
+
+## Components
+
+1. HTML
+2. CSS
+3. JavaScript
+
+## Notes
---
+++ script.js
@@ -0,0 +1,246 @@
+// Get references to HTML elements
+const teamName = document.getElementById("team");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
+
+// Define data for a football team
+const myFavoriteFootballTeam = {
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
+  players: [
+    {
+      name: "Sergio Almirón",
+      position: "forward",
+      number: 1,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Sergio Batista",
+      position: "midfielder",
+      number: 2,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Ricardo Bochini",
+      position: "midfielder",
+      number: 3,
+      isCaptain: false,
+      nickname: "El Bocha",
+    },
+    {
+      name: "Claudio Borghi",
+      position: "midfielder",
+      number: 4,
+      isCaptain: false,
+      nickname: "Bichi",
+    },
+    {
+      name: "José Luis Brown",
+      position: "defender",
+      number: 5,
+      isCaptain: false,
+      nickname: "Tata",
+    },
+    {
+      name: "Daniel Passarella",
+      position: "defender",
+      number: 6,
+      isCaptain: false,
+      nickname: "El Gran Capitán",
+    },
+    {
+      name: "Jorge Burruchaga",
+      position: "forward",
+      number: 7,
+      isCaptain: false,
+      nickname: "Burru",
+    },
+    {
+      name: "Néstor Clausen",
+      position: "defender",
+      number: 8,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "José Luis Cuciuffo",
+      position: "defender",
+      number: 9,
+      isCaptain: false,
+      nickname: "El Cuchu",
+    },
+    {
+      name: "Diego Maradona",
+      position: "midfielder",
+      number: 10,
+      isCaptain: true,
+      nickname: "El Pibe de Oro",
+    },
+    {
+      name: "Jorge Valdano",
+      position: "forward",
+      number: 11,
+      isCaptain: false,
+      nickname: "The Philosopher of Football",
+    },
+    {
+      name: "Héctor Enrique",
+      position: "midfielder",
+      number: 12,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Oscar Garré",
+      position: "defender",
+      number: 13,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Ricardo Giusti",
+      position: "midfielder",
+      number: 14,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Luis Islas",
+      position: "goalkeeper",
+      number: 15,
+      isCaptain: false,
+      nickname: "El loco",
+    },
+    {
+      name: "Julio Olarticoechea",
+      position: "defender",
+      number: 16,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Pedro Pasculli",
+      position: "forward",
+      number: 17,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Nery Pumpido",
+      position: "goalkeeper",
+      number: 18,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Oscar Ruggeri",
+      position: "defender",
+      number: 19,
+      isCaptain: false,
+      nickname: "El Cabezón",
+    },
+    {
+      name: "Carlos Tapia",
+      position: "midfielder",
+      number: 20,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Marcelo Trobbiani",
+      position: "midfielder",
+      number: 21,
+      isCaptain: false,
+      nickname: "Calesita",
+    },
+    {
+      name: "Héctor Zelada",
+      position: "goalkeeper",
+      number: 22,
+      isCaptain: false,
+      nickname: null,
+    },
+  ],
+};
+
+// Freeze the football team object to prevent modifications
+Object.freeze(myFavoriteFootballTeam);
+
+// Destructure properties from the football team object
+const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
+
+// Set text content of HTML elements to corresponding data
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+// Function to render player cards
+const setPlayerCards = (arr = players) => {
+  // Clear existing player cards
+  playerCards.innerHTML = "";
+
+  // Iterate over player array and generate HTML for each player card
+  arr.forEach(({ name, position, number, isCaptain, nickname }) => {
+    const captainText = isCaptain ? "(Captain)" : ""; // Add "(Captain)" text if player is captain
+    const nicknameText = nickname !== null ? nickname : "N/A"; // Display nickname or "N/A" if null
+    const playerCardHTML = `
+      <div class="player-card">
+        <h2>${name} ${captainText}</h2>
+        <p>Position: ${position}</p>
+        <p>Number: ${number}</p>
+        <p>Nickname: ${nicknameText}</p>
+      </div>
+    `;
+    // Append player card HTML to playerCards element
+    playerCards.innerHTML += playerCardHTML;
+  });
+};
+
+// Event listener for dropdown list change
+playersDropdownList.addEventListener("change", (e) => {
+  // Clear player cards when dropdown selection changes
+  playerCards.innerHTML = "";
+
+  // Use a switch statement to determine which players to display based on dropdown selection
+  switch (e.target.value) {
+    case "nickname":
+      // Filter players with a nickname and render player cards
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    // Filter players by position and render player cards
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(
+        players.filter((player) => player.position === "midfielder")
+      );
+      break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
+    case "goalkeeper":
+      setPlayerCards(
+        players.filter((player) => player.position === "goalkeeper")
+      );
+      break;
+    // Render all players by default
+    default:
+      setPlayerCards();
+  }
+});
